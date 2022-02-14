9be00eb5f8ae7cb3e8c2a959a9928f2c6f1ee5e5
taccisum <liaojinfeng@deepexi.com>
2022-02-14 17:03:05 +0800

    feat: provide extensions info for endpoint 'pigeon/info'

--- src/main/java/com/github/taccisum/pigeon/core/spring/endpoints/PigeonEndpoint.java
+++ src/main/java/com/github/taccisum/pigeon/core/spring/endpoints/PigeonEndpoint.java
@@ -4,12 +4,17 @@ import org.pf4j.PluginManager;
 import org.pf4j.PluginWrapper;
 import org.springframework.boot.actuate.endpoint.web.annotation.RestControllerEndpoint;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.Resource;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * actuator endpoint
@@ -32,6 +37,20 @@ public class PigeonEndpoint {
             HashMap<Object, Object> info = new HashMap<>();
             info.put("version", plugin.getDescriptor().getVersion());
             info.put("state", plugin.getPluginState());
+            List extensions = pluginManager.getExtensions(plugin.getPluginId());
+            if (!CollectionUtils.isEmpty(extensions)) {
+                info.put("extensions", extensions.stream()
+                        .map(extension -> {
+                            Map<String, Object> extInfo = new HashMap<>();
+                            extInfo.put("class", extension.getClass());
+                            if (extension instanceof Ordered) {
+                                extInfo.put("order", ((Ordered) extension).getOrder());
+                            }
+                            return extInfo;
+                        })
+                        .collect(Collectors.toList())
+                );
+            }
             plugins.put(plugin.getPluginId(), info);
         }
         map.put("plugins", plugins);
