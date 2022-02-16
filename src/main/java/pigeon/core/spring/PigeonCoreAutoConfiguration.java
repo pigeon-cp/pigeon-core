29b08890e57592d9c0f096f6e56cccdae865d213
taccisum <liaojinfeng@deepexi.com>
2022-02-16 11:09:42 +0800

    chore: scan package 'pigeon.core' on loaded by other app

---
+++ src/main/java/pigeon/core/spring/PigeonCoreAutoConfiguration.java
@@ -0,0 +1,11 @@
+package pigeon.core.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author taccisum - liaojinfeng6938@dingtalk.com
+ * @since 0.2
+ */
+@ComponentScan("pigeon.core")
+public class PigeonCoreAutoConfiguration {
+}
