72a5175220a3532098fc02798c4b76e78acf3b9b
taccisum <liaojinfeng@deepexi.com>
2022-02-22 10:27:52 +0800

    refactor: inherited from BaseDAO instead BaseMapper

--- src/main/java/pigeon/core/dao/MessageTemplateDAO.java
+++ src/main/java/pigeon/core/dao/MessageTemplateDAO.java
@@ -1,11 +1,10 @@
 package pigeon.core.dao;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import pigeon.core.data.MessageTemplateDO;
 
 /**
  * @author taccisum - liaojinfeng6938@dingtalk.com
  * @since 0.1
  */
-public interface MessageTemplateDAO extends BaseMapper<MessageTemplateDO> {
+public interface MessageTemplateDAO extends BaseDAO<MessageTemplateDO> {
 }
